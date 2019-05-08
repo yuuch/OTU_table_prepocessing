38557af328052b3183e3736ce10949db71c30898
yuuch <ywchcs@gmail.com>
2019-05-08 01:22:21 -0400

    almosh finished

---
+++ README.md
@@ -0,0 +1,2 @@
+# preprocessing metagenomic data
+some scipts for preprocessing metagenomic data for machine learning
--- biom_preprocessing.py
+++ biom_preprocessing.py
@@ -41,11 +41,40 @@ class MetaGenomicsPreprocessing(object):
             if ele < thd:
                 remove_columns.append(columns[i])
         self.feature_table = self.feature_table.drop(columns=remove_columns)
-
-
-
-
-    def select_sample(self):
-        pass
-    def explor_metadata(self):
-        pass
+    def select_sample(self,obj_col,wanted_label=None):
+        """ get the index of wanted samples
+        """
+        stats_samples = self.explor_metadata(obj_col)
+        if wanted_label:
+            pass
+        else:
+            wanted_label = stats_samples.keys()
+        index = self.metadata[obj_col].index
+        idxs = []
+        for i,ele in enumerate(self.metadata[obj_col]):
+            if ele in wanted_label:
+                idxs.append(index(i))
+        self.select_idxs = idxs
+        """
+        self.feature_table = self.feature_table.loc[idxs]
+        self.y = self.metadata[obj_col]
+        """
+    def explor_metadata(self, obj_col, plot_flag=False):
+        col_values = self.metadata[obj_col]
+        stats_samples = {}
+        for ele in col_values:
+            if ele in stats_samples:
+                stats_samples[ele] += 1
+            else:
+                stats_samples[ele] = 1
+        if plot_flag:
+            pass
+            # TODO plot a histogram?
+        else:
+            return stats_samples
+    def get_X_y(self,obj_col,wanted_label):
+        self.select_feature()
+        self.select_sample(obj_col)
+        X = self.feature_table.loc[self.select_idxs].values
+        y = self.metadata[obj_col].loc[self.select_idxs]
+        return X,y
