b44ec24a0a88d85391c039dabd267ad66f3f8c1f
yuuch <ywchcs@gmail.com>
2019-07-04 12:18:09 +0000

    edit computing

--- OTU_table_computing.py
+++ OTU_table_computing.py
@@ -1,6 +1,7 @@
 import os
 from multiprocessing import Pool
 import scipy
+import math
 class OTU_Table(object):
     """ read otu table and do some computing.
     """
@@ -11,11 +12,14 @@ class OTU_Table(object):
         self.labels = labels
         if sep_flag:
             self.separate_labels()
+        self.get_abu()
+        self.get_multi_log_pvalue()
+        self.get_multi_GI()
 
-    def get_multi_GI(self,n_processcor=os.cpu_count())
+    def get_multi_GI(self,n_process=os.cpu_count()):
         """ use multiprocess to get multi GI(gini index) paralell
         """
-        with Pool(n_processcor) as p:
+        with Pool(n_process) as p:
             self.GIs = p.map(self.get_single_GI, self.otu_table.columns)
         
 
@@ -25,31 +29,29 @@ class OTU_Table(object):
         above = sum(values)**2
         return above/below
 
-    def get_multi_log_pvalue(self):
-        with Pool(n_processcor) as p:
+    def get_multi_log_pvalue(self, n_process=os.cpu_count()):
+        with Pool(n_process) as p:
             self.log_pvalues = p.map(self.get_single_log_pvalue, self.otu_table.columns)
         
 
     def get_single_log_pvalue(self,column):
         yes_values = self.y_dataframe[column]
         no_values = self.n_dataframe[column]
-        pvalue = scipy.stats.mannwhitneyu(yes_values, no_values)
-        return pvalue
+        pvalue = scipy.stats.mannwhitneyu(yes_values, no_values)[1]
+        return -math.log(pvalue)
 
 
     def separate_labels(self,judge_func=lambda x: x == 'y'):
         """separate index into two group(yes,no)"""
         y_idx = []
         n_idx = []
-        for i,ele in enumerate(lables):
+        for i,ele in enumerate(self.labels):
             if judge_func(ele):
                 y_idx.append(i)
             else:
                 n_idx.append(i)
-        self.y_dataframe = self.dataframe.iloc[y_idx]
-        self.n_dataframe = self.dataframe.iloc[n_idx]
-
-
+        self.y_dataframe = self.otu_table.iloc[y_idx]
+        self.n_dataframe = self.otu_table.iloc[n_idx]
     def get_abu(self):
         self.abus = self.otu_table.mean(axis=0)
         
